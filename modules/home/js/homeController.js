ae4e68d26851cddc2979a6dab15c43a0bd29a082
dotpegaso <dotpegaso@gmail.com>
2018-01-01 00:02:47 +0000

    last commit of the year!

--- modules/home/js/homeController.js
+++ modules/home/js/homeController.js
@@ -2,6 +2,52 @@ angular.module('home', [])
 
 .controller('homeController', function($scope){
     
-    $scope.helloWorld = "Hello World";
+    $scope.email = "email@chimera.com.br";
+    
+    $scope.parceiros = [
+        '/res/temp/partner1.png',
+        '/res/temp/partner2.png',
+        '/res/temp/partner3.png',
+        '/res/temp/partner4.png',
+        '/res/temp/partner5.png',
+        '/res/temp/partner6.png',
+        '/res/temp/partner7.png',
+        '/res/temp/partner8.png',
+        '/res/temp/partner9.png',
+    ]
+    
+    $scope.listaEventos = [
+        {
+            nome: 'Evento bacan',
+            data: '12/06',
+            local: 'local tralala trala, 16 SP',
+            link: 'dotpegaso.com.br'
+        },
+        {
+            nome: 'Evento bacana também',
+            data: '22/09',
+            local: 'local tralala trala, 16 SP',
+            link: 'dotpegaso.com.br'
+        },
+        {
+            nome: 'Evento bacan denovo',
+            data: '07/11',
+            local: 'local tralala trala, 16 SP',
+            link: 'dotpegaso.com.br'
+        },
+        {
+            nome: 'Evento bacan',
+            data: '12/06',
+            local: 'local tralala trala, 16 SP',
+            link: 'dotpegaso.com.br'
+        },
+        {
+            nome: 'Evento bacana também',
+            data: '22/09',
+            local: 'local tralala trala, 16 SP',
+            link: 'dotpegaso.com.br'
+        },
+        
+    ]
     
 })
