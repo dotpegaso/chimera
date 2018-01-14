cc4d9c84af304f92c88e3667f175ce23c6de1b3f
dotpegaso <dotpegaso@gmail.com>
2018-01-14 18:24:28 +0000

    removed jquery, fixed some css bugs, add new information

--- modules/home/js/homeController.js
+++ modules/home/js/homeController.js
@@ -1,80 +1,30 @@
 angular.module('home', [])
 
-.controller('homeController', function($scope){
+.controller('homeController', function($scope, $location, $anchorScroll){
     
-    $scope.email = "email@chimera.com.br";
+    $scope.email = "contato@chimeramedia.com.br";
     
     $scope.parceiros = [
         {
             img: '/res/parceiros/hrtbox.png',
             link: 'https://hrtbox.com.br'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
-        },
-        {
-            img:'/res/temp/partner.png',
-            link: '#'
         }
     ]
     
     $scope.listaEventos = [
         {
-            nome: 'Evento bacan',
-            data: '12/06',
-            local: 'local tralala trala, 16 SP',
-            link: 'dotpegaso.com.br'
-        },
-        {
-            nome: 'Evento bacana também',
-            data: '22/09',
-            local: 'local tralala trala, 16 SP',
-            link: 'dotpegaso.com.br'
-        },
-        {
-            nome: 'Evento bacan denovo',
-            data: '07/11',
-            local: 'local tralala trala, 16 SP',
-            link: 'dotpegaso.com.br'
-        },
-        {
-            nome: 'Evento bacan',
-            data: '12/06',
-            local: 'local tralala trala, 16 SP',
-            link: 'dotpegaso.com.br'
-        },
-        {
-            nome: 'Evento bacana também',
-            data: '22/09',
-            local: 'local tralala trala, 16 SP',
-            link: 'dotpegaso.com.br'
-        },
-        
+            nome: 'SAVE THE DATE',
+            data: 'MARÇO',
+            local: 'Em breve',
+            link: ''
+        }
     ]
+    
+    $scope.scroll = function(elem) {
+       var id = $location.hash();
+        $location.hash(elem);
+        $anchorScroll();
+        $location.hash(id);
+     };
     
 })
